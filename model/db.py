d059e10f8c37afd92fde0a9c5f6b1559ac9e907f
adam <adameve1981zero@gmail.com>
2024-01-30 12:46:07 +0800

    query different sets
    train, validate, test

--- model/db.py
+++ model/db.py
@@ -3,7 +3,8 @@ import psycopg
 
 class Database(object):
     def __init__(self):
-        self.conn = psycopg.connect("dbname=capstone user=capstone password=123456 host=localhost port=5432")
+        self.conn: psycopg.connection.Connection = psycopg.connect("dbname=capstone user=capstone password=123456 "
+                                                                   "host=localhost port=5432")
 
     def get_connection(self):
         return self.conn
--- model/stocks.py
+++ model/stocks.py
@@ -1,6 +1,18 @@
-from typing import Tuple, Optional
+from enum import Enum
+from typing import Optional
+from string import Template
+
+from psycopg import Cursor
+from psycopg.rows import Row
 
 from model.db import Database
+
+
+class DataFragment(Enum):
+    TRAINING = "training"
+    TESTING = "testing"
+    VALIDATION = "validation"
+    ALL = "all"
 
 
 class Stocks(Database):
@@ -18,7 +30,13 @@ class Stock(Database):
     def __init__(self, stk_id: str):
         super().__init__()
         self.stk_id: str = stk_id
+
         self._count: Optional[int] = None
+        self.boundary_training_validating = None
+        self.boundary_validating_testing = None
+
+        self.query_template = Template(
+            f"select * from stock where stkcd = '000001' order by trddt offset $offset limit $limit")
 
     @property
     def count(self) -> int:
@@ -27,15 +45,47 @@ class Stock(Database):
                 f"select count(*) from stock where stkcd = '{self.stk_id}'"
             )
             self._count = self.get_connection().execute(query).fetchone()[0]
+        if self.boundary_validating_testing is None and self.boundary_training_validating is None:
+            self._calculate_boundaries()
         return self._count
 
-    def fetch(self, r: Tuple[int, int] = None):
+    def _calculate_boundaries(self) -> None:
+        self.boundary_training_validating = int(self._count * 0.7)
+        self.boundary_validating_testing = int(self._count * (0.7 + 0.2))
+
+    def fetch(self, fragment: DataFragment) -> Cursor[Row]:
         """
-        Fetch individual stock data from database
-        :param r: percentage of stocks to fetch, must be integer between 0 and 100
+        Fetch individual stock data from a database
+        :param fragment:
         :return: a psql cursor
         """
-        if r is None:
-            return self.get_connection().execute("select * from stock")
-        else:
-            pass
+        _ = self.count
+        match fragment:
+            case DataFragment.TRAINING:
+                return self.get_connection().execute(
+                    self.query_template.substitute(
+                        offset=0,
+                        limit=self.boundary_training_validating - 1
+                    )
+                )
+            case DataFragment.VALIDATION:
+                return self.get_connection().execute(
+                    self.query_template.substitute(
+                        offset=self.boundary_training_validating,
+                        limit=self.boundary_validating_testing - 1
+                    )
+                )
+            case DataFragment.TESTING:
+                return self.get_connection().execute(
+                    self.query_template.substitute(
+                        offset=self.boundary_validating_testing,
+                        limit='all'
+                    )
+                )
+            case DataFragment.ALL:
+                return self.get_connection().execute(
+                    self.query_template.substitute(
+                        offset=0,
+                        limit='all'
+                    )
+                )
