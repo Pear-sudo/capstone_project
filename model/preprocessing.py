250285a8881cbbeca3e7a7b84653f2db84c48702
adam <adameve1981zero@gmail.com>
2024-02-14 16:37:47 +0800

    db-based fetching logic
    I'll complete this if I have extra time. Now I need to train the model as quickly as possible using numpy to meet the ddl.

---
+++ model/preprocessing.py
@@ -0,0 +1,19 @@
+class Normalizer:
+    def __init__(self, table: str, ordering_column: str, columns: list[str], std_columns: list[str]):
+        self.moving_average_window = 7
+        self.moving_average_template = f"""
+with stats as (select opnprc,
+                      avg(opnprc) over win    as avg,
+                      stddev(opnprc) over win as std
+               from {table}
+               where stkcd = '000001'
+               window win as (order by {ordering_column} rows between {self.moving_average_window - 1} preceding and current row))
+select case
+           when std = 0 then null
+           else (opnprc - avg) / std
+           end
+from stats
+"""
+
+    def sql(self):
+        pass
