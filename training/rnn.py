83c0c9edf0b98b7f7ff072a23e20e1138d7e84d8
adam <adameve1981zero@gmail.com>
2024-02-18 18:11:46 +0800

    rnn

---
+++ training/rnn.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+lstm_model = tf.keras.models.Sequential([
+    # Shape [batch, time, features] => [batch, time, lstm_units]
+    tf.keras.layers.LSTM(32, return_sequences=True),
+    # Shape => [batch, time, features]
+    tf.keras.layers.Dense(units=1)
+])
