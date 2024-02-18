83f2b7dc05153a0a02a9ec13c8c86d86fda4e761
adam <adameve1981zero@gmail.com>
2024-02-18 21:40:25 +0800

    bug fix: the model should return a new instance every time it is called

--- training/cnn.py
+++ training/cnn.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 
 CONV_WIDTH = 3
 
-conv_model = tf.keras.Sequential([
-    tf.keras.layers.Conv1D(filters=32,
-                           kernel_size=(CONV_WIDTH,),
-                           activation='relu'),
-    tf.keras.layers.Dense(units=32, activation='relu'),
-    tf.keras.layers.Dense(units=1),
-])
+
+def get_cnn():
+    return tf.keras.Sequential([
+        tf.keras.layers.Conv1D(filters=32,
+                               kernel_size=(CONV_WIDTH,),
+                               activation='relu'),
+        tf.keras.layers.Dense(units=32, activation='relu'),
+        tf.keras.layers.Dense(units=1),
+    ])
--- training/rnn.py
+++ training/rnn.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 
-lstm_model = tf.keras.models.Sequential([
-    # Shape [batch, time, features] => [batch, time, lstm_units]
-    tf.keras.layers.LSTM(32, return_sequences=True),
-    # Shape => [batch, time, features]
-    tf.keras.layers.Dense(units=1)
-])
+
+def get_rnn():
+    return tf.keras.models.Sequential([
+        # Shape [batch, time, features] => [batch, time, lstm_units]
+        tf.keras.layers.LSTM(32, return_sequences=True),
+        # Shape => [batch, time, features]
+        tf.keras.layers.Dense(units=1)
+    ])
