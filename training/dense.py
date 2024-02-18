e3a5e9537eb157b5875a6027278cd7cb70b0819f
adam <adameve1981zero@gmail.com>
2024-02-18 18:11:01 +0800

    multi step dense

--- training/dense.py
+++ training/dense.py
@@ -7,3 +7,16 @@ def get_dense():
         tf.keras.layers.Dense(units=64, activation='relu'),
         tf.keras.layers.Dense(units=1)
     ])
+
+
+def get_multi_step_dense():
+    return tf.keras.Sequential([
+        # Shape: (time, features) => (time*features)
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(units=32, activation='relu'),
+        tf.keras.layers.Dense(units=32, activation='relu'),
+        tf.keras.layers.Dense(units=1),
+        # Add back the time dimension.
+        # Shape: (outputs) => (1, outputs)
+        tf.keras.layers.Reshape([1, -1]),
+    ])
