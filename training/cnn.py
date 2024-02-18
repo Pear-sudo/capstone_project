26d511e247faef289775d51f77b8463a134efbc5
adam <adameve1981zero@gmail.com>
2024-02-18 18:11:07 +0800

    cnn

---
+++ training/cnn.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+CONV_WIDTH = 3
+
+conv_model = tf.keras.Sequential([
+    tf.keras.layers.Conv1D(filters=32,
+                           kernel_size=(CONV_WIDTH,),
+                           activation='relu'),
+    tf.keras.layers.Dense(units=32, activation='relu'),
+    tf.keras.layers.Dense(units=1),
+])
