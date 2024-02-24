b3030dfe43f8e7be17525748b315ff826240295b
adam <adameve1981zero@gmail.com>
2024-02-24 10:38:40 +0800

    combine the loader with preprocessor
    they are closely related and share the same strategy

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -63,6 +63,34 @@ class Preprocessor:
     def __init__(self, strategy: LoadingStrategy):
         self.strategy = strategy
 
+    def load_dataset(self, path: str) -> pd.DataFrame:
+        df = pd.read_csv(path)
+        return df
+
+    def load_normalized_dataset(self, path: str) -> Optional[pd.DataFrame]:
+        df = self.load_dataset(path)
+        df = self.normalize_dataset(df)
+        return df
+
+    def split_to_dataframes(self, df: pd.DataFrame, ratio: tuple[float, float, float] = (0.7, 0.2, 0.1)) \
+            -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+        tolerance = 1e-6
+        if not abs(sum(ratio) - 1) < tolerance:
+            raise ValueError(f"The sum of ratio must be equal to 1, current sum: {sum(ratio)}")
+
+        n = len(df)
+        cut_train_val = int(n * ratio[0])
+        cut_val_test = int(n * (ratio[0] + ratio[1]))
+
+        train_df: pd.DataFrame = df[:cut_train_val]
+        val_df: pd.DataFrame = df[cut_train_val:cut_val_test]
+        test_df: pd.DataFrame = df[cut_val_test:]
+
+        if train_df is None or val_df is None or test_df is None:
+            raise RuntimeError('Dataset is empty for unknown reasons.')
+
+        return train_df, val_df, test_df
+
     def normalize_date(self, df: pd.DataFrame, date_column: str) -> pd.DataFrame:
         sample = df[date_column].iloc[0]
         if not self.is_valid_date(sample):
