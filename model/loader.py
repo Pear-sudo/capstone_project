db4d555a512414e060d07c52a2affc581e816240
adam <adameve1981zero@gmail.com>
2024-02-28 14:19:55 +0800

    find all csv files

--- model/loader.py
+++ model/loader.py
@@ -1,13 +1,46 @@
+import os
 import zipfile
 from os import PathLike
 from pathlib import Path
+from typing import Callable
 
 
-def unzip(file: str) -> Path:
+def unzip_all(zip_files: list[Path]) -> bool:
+    non_zip_files = [str(file) for file in zip_files if not is_zipfile(file)]
+    if non_zip_files:
+        raise ValueError("Those files are not zip:\n {}".format('\n'.join(non_zip_files)))
+
+    for zip_file in zip_files:
+        print('Unzipping {}'.format(zip_file.name))
+        unzip(zip_file)
+        print('Unzipped {}'.format(zip_file.name))
+
+    return False
+
+
+def filter_zip_files(directory: PathLike | str) -> list[Path]:
+    return filter_files(directory, lambda file: file.endswith('.zip'))
+
+
+def filter_files(directory: PathLike | str, examiner: Callable[[str], bool]) -> list[Path]:
+    directory = Path(directory)
+    if not directory.is_dir():
+        raise ValueError(f'{directory} is not a directory')
+    target_paths = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # I choose to pass str to examiner for performance considerations
+            # after all, most of these files will be discarded.
+            if examiner(file):
+                target_paths.append(Path(os.path.join(root, file).__str__()))
+    return target_paths
+
+
+def unzip(file: str | PathLike) -> Path:
     zip_path = Path(file)
     if not zip_path.is_file():
         raise FileNotFoundError()
-    if not zip_path.suffix == '.zip':
+    if not is_zipfile(zip_path):
         raise ValueError(f'File {file} is not a zip file')
 
     parent = zip_path.parent
@@ -29,6 +62,10 @@ def unzip(file: str) -> Path:
         zip_ref.extractall(directory)
 
     return directory
+
+
+def is_zipfile(file: Path) -> bool:
+    return file.suffix == '.zip'
 
 
 def examine_csmar_dir(directory: PathLike | str) -> bool:
@@ -71,5 +108,6 @@ def examine_csmar_dir(directory: PathLike | str) -> bool:
 
 if __name__ == '__main__':
     pass
+    filter_zip_files(r'/Users/a/playground/freestyle/')
     # examine_csmar_dir(r'/Users/a/playground/freestyle/China Economic Research Series/Macroeconomic/Gdp/Quarterly Gross Domestic Product181521220')
     # unzip(r'/Users/a/playground/freestyle/China Economic Research Series/Macroeconomic/Gdp/Quarterly Gross Domestic Product181521220.zip')
