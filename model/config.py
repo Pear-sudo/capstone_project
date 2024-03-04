31446145615f5bc2848e5c4364a74120266c95f4
adam <adameve1981zero@gmail.com>
2024-03-04 21:14:28 +0800

    find config files with checking

--- model/config.py
+++ model/config.py
@@ -1,3 +1,7 @@
+import copy
+import datetime
+import shutil
+
 import yaml
 
 from loader import *
@@ -9,6 +13,11 @@ class DataConfigLayout:
     configured: str = 'configured'
     ignored: str = 'ignored'
     backup: str = 'backup'
+
+    def __post_init__(self):
+        self.configured: Path = self.root.joinpath(self.configured)
+        self.ignored: Path = self.root.joinpath(self.ignored)
+        self.backup: Path = self.root.joinpath(self.backup)
 
 
 def make_layout(layout: DataConfigLayout):
@@ -22,9 +31,30 @@ def make_layout(layout: DataConfigLayout):
             directory.mkdir()
 
 
+def make_timestamp() -> str:
+    return datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+
+
+def iter_dir(directory: Path,
+             if_file: Callable[[Path], None],
+             if_dir: Callable[[Path], None],
+             if_else: Callable[[Path], None]):
+    for path in directory.iterdir():
+        if path.is_file():
+            if_file(path)
+        elif path.is_dir():
+            if_dir(path)
+        else:
+            if_else(path)
+
+
 class DataConfig:
     def __init__(self, layout: DataConfigLayout):
         self.layout: DataConfigLayout = layout
+
+        self.unconfigured: list[Path] = []
+        self.configured: list[Path] = []
+        self.ignored: list[Path] = []
 
     def make_config(self, datas: list[CsmarData]):
         suffix = '.yaml'
@@ -39,11 +69,47 @@ class DataConfig:
     def clean_config(self):
         pass
 
-    def read_config(self):
+    def load_config(self):
         pass
+
+    def _find_config_files(self):
+        def raise_error(p):
+            raise RuntimeError(f"Config dir damaged: do not expect {p}")
+
+        def if_file(append_to: list) -> Callable[[Path], None]:
+            def f(p: Path):
+                if p.is_file():
+                    if p.suffix == '.yaml':
+                        append_to.append(p)
+                    else:
+                        raise_error(p)
+
+            return f
+
+        def if_dir(p: Path):
+            legitimate_dirs = self.layout.__dict__.values()
+            if p not in legitimate_dirs:
+                raise_error(p)
+
+        iter_dir(self.layout.root, if_file(self.unconfigured), if_dir, raise_error)
+
+        iter_dir(self.layout.configured, if_file(self.configured), raise_error, raise_error)
+
+        iter_dir(self.layout.ignored, if_file(self.ignored), raise_error, raise_error)
+
+        dic: dict = copy.deepcopy(self.layout.__dict__)
+        del dic['root']
+        for path in dic.values():
+            path: Path = path
+            if not (path.is_dir() and path.exists()):
+                raise RuntimeError(f"Config dir damaged: do not found {path}")
+
+    def make_backup(self):
+        file_path = self.layout.backup
+        shutil.make_archive(dry_run=True, base_name=make_timestamp(), root_dir=self.layout.backup, )
 
 
 if __name__ == '__main__':
     # make_layout(DataConfigLayout(Path('./config/data')))
-    csmar_datas = load_csmar_data(r'/Users/a/playground/freestyle/')
-    DataConfig(DataConfigLayout(Path('./config/data'))).make_config(csmar_datas)
+    # csmar_datas = load_csmar_data(r'/Users/a/playground/freestyle/')
+    DataConfig(DataConfigLayout(Path('./config/data'))).make_backup()
